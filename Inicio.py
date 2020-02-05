b9e0cbd54d44b0871960f82ba13854b0e7bcf0af
Leandro <lsferreiar934@gmail.com>
2020-02-05 20:18:45 -0300

    Correção

--- Inicio.py
+++ Inicio.py
@@ -3,7 +3,7 @@ import Menu
 print('\n\n')
 print('by Leandro Ferreira')
 print('-=' * 45)
-print('\t\t\t### BEM VINDO AO CRIADO DE BANCO DE DADOS ###\n')
+print('\t\t\t### BEM VINDO AO CRIADOR DE BANCO DE DADOS ###\n')
 print('\t\t\t\t\tWARNING!!\n')
 print('\t\t*CERTIFIQUE - SE DE TER INSTALADO OS SEGUINTES PROGRAMAS:')
 print('\t\tXAMPP: https://www.apachefriends.org/pt_br/download.html\n')
